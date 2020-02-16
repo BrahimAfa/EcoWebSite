ed3f4fbfbc292f2e5ead42cfedeb47fc2c67aabc
Brahim Afassy <brahimafassy@gmail.com>
2020-02-16 17:34:55 +0100

    Set up JRebel 'rebel.xml'

--- src/com/eco/Servlets/Login.java
+++ src/com/eco/Servlets/Login.java
@@ -27,7 +27,7 @@ public class Login extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served hello at: ").append(request.getContextPath());
+		response.getWriter().append("Heello WITh Jrebel ").append(request.getContextPath());
 	}
 
 	/**
