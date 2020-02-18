b417a9615721a15c03b54915a77a2b2873ec2c87
Brahim Afassy <brahimafassy@gmail.com>
2020-02-18 19:06:46 +0100

    Modified Web Configuration File

--- src/com/eco/Utils/Helpers.java
+++ src/com/eco/Utils/Helpers.java
@@ -1,5 +1,10 @@
 package com.eco.Utils;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import com.eco.Models.Product;
+
 public class Helpers {
 	public static boolean LoginValidation(String email,String Password) {
 		if(!email.matches("^\\w+@\\w+\\.\\w+{2,}$") || email.equals("")|| email==null) {
@@ -9,5 +14,15 @@ public class Helpers {
 			return false;
 		}
 		return true;
+	}
+	
+	public static String getProductAsHtmlTableRow(List<Product> P) {
+		StringBuilder str = new StringBuilder();
+		P.forEach(p-> 
+		str.append(String.format(
+				"<tr><th scope='row'>%d</th> <td>%s</td> <td>%d</td></td></tr>", 
+				p.getId(),p.getName(),p.getPrice()))
+				);
+		return str.toString();
 	}
 }
